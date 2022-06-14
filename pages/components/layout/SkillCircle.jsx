0a5731ce77ded534a03cbe06cd9b08569bf7edf1
Kevin Volkel <kevinator900@gmail.com>
2022-06-14 00:30:04 -0700

    made skillCircles change with the percentage

--- pages/components/layout/SkillCircle.jsx
+++ pages/components/layout/SkillCircle.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const SkillCircle = ({ skill, percentage }) => {
-  return <>
-    <div className="skill-container">
-      <div className="skill-name">{skill}</div>
-      <div className="skill-percentage">{percentage}%</div>
-    </div>
-  </>
-}
 
-export default SkillCircle
+  const findClipPath = (perc) => {
+    const ranges = [
+      [100, 87.5], //! clip-path: polygon(50% 0%, 50% 50%, [50 - 100]% 0, 100% 0, 100% 100%, 0 100%, 0 0);
+      [87.5, 62.5], //! clip-path: polygon(0 0, 50% 0, 50% 50%, 100% [0 - 50]%, 100% 100%, 0 100%);
+      [62.5, 37.5], //! clip-path: polygon(50% 0%, 50% 50%, [0 - 100]% 100%, 0 100%, 0 0);
+      [37.5, 12.5], //! clip-path: polygon(0 0, 50% 0, 50% 50%, 0 [0 - 100]%);
+      [12.5, 0], //! clip-path: polygon(50% 0%, [0 - 50]% 0, 50% 50%);
+    ];
+    
+
+    let range = [];
+    ranges.map((item) => {
+      if (range.length == 0 && item[1] < perc) range = item;
+    });
+
+    switch(range[0]) {
+      case 100: 
+        return `polygon(50% 0%, 50% 50%, ${50 + 50 * ( (100 - perc) / 12.5)}% 0, 100% 0, 100% 100%, 0 100%, 0 0)`
+      case 87.5:
+        return `polygon(0 0, 50% 0, 50% 50%, 100% ${100 * ( (87.5 - perc) / 25)}%, 100% 100%, 0 100%)`
+      case 62.5:
+        return `polygon(50% 0%, 50% 50%, ${100 - 100 * ( (62.5 - perc) / 25)}% 100%, 0 100%, 0 0)`
+      case 37.5:
+        return `polygon(0 0, 50% 0, 50% 50%, 0 ${100 - 100 * ( (37.5 - perc) / 25)}%)`
+      case 12.5: 
+        return `polygon(50% 0%, ${50 * ( (12.5 - perc) / 12.5)}% 0, 50% 50%)`
+      default:
+        return 'polygon(0 0, 0 0)'
+    }
+
+  };
+
+  return (
+    <>
+      <div className="skill-container">
+        <div className="progress" style={{clipPath: findClipPath(percentage)}}></div>
+        <div className="skill-name">{skill}</div>
+        <div className="skill-percentage">{percentage}%</div>
+      </div>
+    </>
+  );
+};
+
+export default SkillCircle;
--- pages/components/sections/Skills.jsx
+++ pages/components/sections/Skills.jsx
@@ -5,6 +5,11 @@ const Skills = () => {
   return <div className="skills-container">
     <div className="section-title">Skills</div>
     <SkillCircle skill="html" percentage={95}/>
+    <SkillCircle skill="css" percentage={75}/>
+    <SkillCircle skill="js" percentage={85}/>
+    <SkillCircle skill="next" percentage={65}/>
+    <SkillCircle skill="scss" percentage={70}/>
+    <SkillCircle skill="DB" percentage={85}/>
   </div>
 }
 
